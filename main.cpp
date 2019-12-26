2308bbe2bfd36f7207ce5e936acd81bd51e29b25
radek <radoslaw.pacholec@wp.pl>
2019-12-26 20:34:23 +0100

    Add new files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int m)
+{
+    if (m==0) return 3;
+    else return f(m-1)+2;
+}
+
+long int potega(int p, int w)
+{
+    if (w==0) return 1;
+    else return p*potega(p,w-1);
+}
+int main()
+{
+    cout << f(3) << endl;
+    cout << potega (3,4);
+    return 0;
+}
